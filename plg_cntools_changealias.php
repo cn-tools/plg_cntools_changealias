fdb79c4d4830f0d897cbd3f91d0a39f0dd21d8d1
cn-tools <cn-tools@gmx.at>
2015-12-15 12:09:09 +0100

    language, additional params
    * change some little langauge issues
    * add param to specify include/exclude components

--- plg_cntools_changealias.php
+++ plg_cntools_changealias.php
@@ -26,28 +26,62 @@ class PlgContentPlg_CNTools_ChangeAlias extends JPlugin
 		parent::__construct( $subject, $config );
 	}
 	
-	//--------------------------------------------------------------
+	//-------------------------------------------------------------------------
 	public function onContentBeforeSave($context, $article, $isNew) 
 	{
 		if (is_object($article) and property_exists($article, 'alias'))
 		{
 			if (($isNew) or ($this->params->get('onlynew')=='0'))
 			{
-				$article->alias = $this->transferChars($article->alias);
+				if ($this->checkContext($context))
+				{
+					$article->alias = $this->transferChars($article->alias);
+				}
 			}
 		}
 		return true;
 	}
 
-	//--------------------------------------------------------------
-	protected function transferChars($basestr)
+	//-------------------------------------------------------------------------
+	protected function checkContext($context)
+	{
+		$lResult = true;
+		if ($this->params->get('contextyesno', '0') == '1')
+		{
+			//-- include part ---------------------------------------------
+			$lResult = $this->isContextFound($context);
+		}
+		elseif ($this->params->get('contextyesno', '0') == '2') 
+		{
+			//-- exclude part ---------------------------------------------
+			$lResult = !$this->isContextFound($context);
+		}
+		
+		return $lResult;
+	}
+	//-------------------------------------------------------------------------
+	protected function isContextFound($context)
+	{
+		$lValues = array_map('trim', explode("\n", $this->params->get('context')));
+
+		foreach ($lValues as $lValue){
+			//-- check if lValue is in context ---------------------------------
+			if (stripos($context, $lValue) !== false) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+	//-------------------------------------------------------------------------
+	protected function transferChars($alias)
 	{
 		//-- maybe transfer alias into upper/lower-case characters
 		$doUpLow = $this->params->get('uplow');
 		if ($doUpLow == '1') {
-			$basestr = mb_strtoupper($basestr);
+			$alias = mb_strtoupper($alias);
 		} elseif ($doUpLow == '2') {
-			$basestr = mb_strtolower($basestr);
+			$alias = mb_strtolower($alias);
 		}
 		
 		//-- maybe transfer alias characters
@@ -56,22 +90,30 @@ class PlgContentPlg_CNTools_ChangeAlias extends JPlugin
 
 			//-- rework every item for alias
 			foreach ($lValues as &$lSubArray){
-				// special transfer '[space]' to one space ' '
-				$lSubArray = str_ireplace('[space]' , ' ', $lSubArray);
-				// special transfer '[nothing]' to empty string ''
-				$lSubArray = str_ireplace('[nothing]' , '', $lSubArray);
+				$lSubArray = trim($lSubArray);
+				$lSubArray = trim($lSubArray, '=');
+				if (stripos($lSubArray, '=') !== false) {
+					// special transfer '[space]' to one space ' '
+					$lSubArray = str_ireplace('[space]' , ' ', $lSubArray);
+					// special transfer '[nothing]' to empty string ''
+					$lSubArray = str_ireplace('[nothing]' , '', $lSubArray);
 
-				$workArray = explode('=', $lSubArray); 
-				if ($this->params->get('casesensitiv')){
-					// use case sensitiv chars
-					$basestr = str_replace($workArray[0] , $workArray[1], $basestr); 
-				} else {
-					// do not use case sensitive chars
-					$basestr = str_ireplace($workArray[0] , $workArray[1], $basestr); 
+					$workArray = explode('=', $lSubArray); 
+					
+					if ($this->params->get('casesensitiv')){
+						// use case sensitiv chars
+						$alias = str_replace($workArray[0] , $workArray[1], $alias); 
+					} else {
+						// do not use case sensitive chars
+						$alias = str_ireplace($workArray[0] , $workArray[1], $alias); 
+					}
 				}
 			}
+
+			// Trim white spaces at beginning and end of alias
+			$alias = trim($alias);
 		}
-		return $basestr;
+		return $alias;
 	}
 }
 ?>
